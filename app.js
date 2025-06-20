c08811cd9a92a189ed1e5cffaf6cc02da24219ab
mcavallaro23 <cavallaromarcos@gmail.com>
2025-06-20 15:16:23 -0300

    Update redirectTo URL for production

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ async function loginWithGoogle() {
         const { data, error } = await supabase.auth.signInWithOAuth({
             provider: 'google',
             options: {
-                redirectTo: 'http://localhost:5500'
+                redirectTo: 'https://mcavallaro23.github.io/Trainning-app/'
             }
         });
         
